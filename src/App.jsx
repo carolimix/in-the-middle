5f10630a20d062b03292d77621a2759772433274
Carolina <carolina.acuna90@gmail.com>
2024-06-11 17:54:54 +0200

    form added to mapbox branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Mapbox from "./components/Mapbox";
-import Mapbox from "../src/components/Mapbox";
+import Mapbox from "./components/Mapbox";
+import AddForm from "./components/AddForm";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Mapbox />} />
-          {/* <Route path="/map" element={<Map />} /> */}
+          <Route path='/' element={<Mapbox />} />
+          <Route path='/add' element={<AddForm />} />
         </Routes>
       </Router>
     </>
