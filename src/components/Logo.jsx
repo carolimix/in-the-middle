5d7dd3ecac3678eea8fceedc80f4e9773d2cf956
Daiane Lohse <daianelohse@gmail.com>
2024-06-16 20:39:22 +0200

    git branch -m old-name new-nameFilter by benches, toilet and card / the json was also increased

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+function Logo() {
+
+    return (
+        <img src="src/assets/logo.png" alt="" width="200px" height="100px"></img>
+      );
+    }
+    
+    export default Logo;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,33 +1,46 @@
 import { SearchBox } from "@mapbox/search-js-react";
 import { useEffect, useState } from "react";
 import Checkbox from "./Checkbox";
-const tokenMapBox = import.meta.env.VITE_ACCESS_TOKEN
+import Logo from "./Logo";
+import "../Search.css";
+const tokenMapBox = import.meta.env.VITE_ACCESS_TOKEN;
 
 // eslint-disable-next-line react/prop-types
 export default function Search({ addMarkersOnMap, handleCheckBox }) {
   const [value, setValue] = useState("");
   function handleSearch(e) {
-    
     //get the coordanates of the adreess typed
     setValue((prevState) => e.features[0].properties.full_address);
-    addMarkersOnMap(e.features[0].geometry.coordinates,value);
+    addMarkersOnMap(e.features[0].geometry.coordinates, value);
   }
 
   const handleInnerCheckBox = (benches, toilet, card) => {
-   return handleCheckBox(benches, toilet, card);
-  }
+    return handleCheckBox(benches, toilet, card);
+  };
 
-  
   return (
-    <form>
-      <SearchBox
-        value={value}
-        onRetrieve={handleSearch}
-        accessToken={tokenMapBox}
-      />
+    <form className="mainForm">
+      <div className="flex-container">
+      
+        <div className="item1">
+          <Logo />
+        </div>
 
-    <Checkbox handleCheckBox={handleInnerCheckBox} />
+        <div className="item2">
+          <SearchBox
+          
+            className="inputSearch"
+            value={value}
+            onRetrieve={handleSearch}
+            accessToken={tokenMapBox}
+          />
+          </div>
 
+          <div className="item3">
+        
+          <Checkbox handleCheckBox={handleInnerCheckBox} />{" "}
+        </div>
+      </div>
     </form>
   );
 }
