940db767abc36dc104ef404b9c457d3036810c81
Daiane Lohse <daianelohse@gmail.com>
2024-06-23 22:39:32 +0200

    updates:  add spati styles + validation

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
+
+
+
 function Logo() {
 
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    console.log("etrei");
+    navigate("/");
+  };
     return (
-        <img src="src/assets/logo.png" alt="" width="200px" height="100px"></img>
+        <img onClick={() => goToHome()} src="src/assets/logo.png" alt="" width="200px" height="100px"></img>
       );
     }
     
--- src/components/NewSpati.jsx
+++ src/components/NewSpati.jsx
@@ -1,8 +1,22 @@
+import "../index.css";
+import { useNavigate } from "react-router-dom";
+
+
 function NewSpati() {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    console.log("etrei");
+    navigate("/");
+  };
+
     return (
-      <div>
-        <h1>We received your info. Cheers for collaborating! :beers:</h1>
-      </div>
+      <div id="formSuccessSent">
+        <h1 className="blue">We received your info. <br /> <span className="red">Cheers for collaborating! 🍻</span> </h1>
+      <div id="searchSpati">
+       <a  onClick={() => goToHome()}>🔍 Search a Späti</a>
+       </div>
+    </div>
     )
   }
 
