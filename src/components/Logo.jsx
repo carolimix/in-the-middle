0066d7d057007171f7e25fc1f4ed95ac4857ef15
Priscila Uchoa <priscilauchoaa@gmail.com>
2024-06-25 15:27:06 +0200

    fix logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,20 +1,19 @@
 import { useNavigate } from "react-router-dom";
+import LogoPNG from '../assets/logo.png'
+
+
 
 function Logo() {
+
   const navigate = useNavigate();
 
   const goToHome = () => {
+    console.log("etrei");
     navigate("/");
   };
   return (
-    <div
-      onClick={() => goToHome()}
-      className="img-logo"
-      alt=""
-      width="200px"
-      height="100px"
-    ></div>
-  );
-}
-
-export default Logo;
+    <img onClick={() => goToHome()} src={LogoPNG} alt="" width="200px" height="100px"></img>
+      );
+    }
+    
+    export default Logo;
